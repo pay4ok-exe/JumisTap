942ddb69e8809031624d23b77fe13ced3ce73518
Akim Ospan <ospanakim05@gmail.com>
2024-06-09 14:17:29 +0500

    :)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    document.body.style.zoom = "100%";
-}
+    document.body.style.zoom = "125%";
+};
 
 // window.addEventListener('load', function() {
 //     document.body.style.zoom = '100%';
